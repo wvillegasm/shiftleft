288e51272fc122d67aaa360793de5a74bdf2449e
Wilfredo Villegas <wivisisorg@gmail.com>
2015-03-21 01:40:09 -0400

    Initial multimodule

---
+++ shiftleft-dao/src/main/java/com/itsoft/slt/dao/App.java
@@ -0,0 +1,13 @@
+package com.itsoft.slt.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ shiftleft-logic/src/main/java/com/itsoft/slt/service/App.java
@@ -0,0 +1,13 @@
+package com.itsoft.slt.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
